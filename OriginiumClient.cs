b2a7e4a96531b16a551eefbb97353dc3b166aa76
akgamerz790 <rockdgyt352@gmail.com>
2026-06-15 06:18:27 +0700

    Initial Commit - Awakening.

---
+++ OriginiumClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EndfieldClient;
+
+internal class OriginiumClient
+{
+    
+    public static async Task Main(string[] args)
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Endfield Client
+ - Minecraft Network Library project
+
+<hr>
+
+### **ENDFIELD INDUSTRIES LICENSED**
+
+
+<hr>
+
+<div align="center">END OF DOCUMENT</div>
