475f9bd35c78a58f7d1461c5632c7c2e04d18e17
akgamerz790 <rockdgyt352@gmail.com>
2026-06-15 17:47:28 +0700

    Upgrades

---
+++ AIC/AICDB/AICDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EndfieldClient.AIC.AICDB;
+
+internal static class AICDatabase
+{
+    public static int _ProtocolVersion = 775;
+}
